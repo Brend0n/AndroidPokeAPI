cc7e0407a0f1b4e41708f115628736d12fcb33b0
Brendon BOUZAS <brendon.bouzas@ymail.com>
2019-03-22 12:45:59 +0900

    implemented view elements with a listener in the search imput

--- app/src/main/java/com/brendon/myapplication/MainActivity.java
+++ app/src/main/java/com/brendon/myapplication/MainActivity.java
@@ -2,12 +2,47 @@ package com.brendon.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "MainActivity";
+
+    EditText search;
+    TextView name, id;
+    ImageView avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search = findViewById(R.id.et_search);
+        name = findViewById(R.id.tv_Name);
+        id = findViewById(R.id.tv_ID);
+        avatar = findViewById(R.id.img_Avatar);
+
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
     }
 }
